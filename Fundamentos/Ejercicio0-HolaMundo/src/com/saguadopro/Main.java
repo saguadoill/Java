7034e2024f003c4674f45076813ab924ae5868ff
sergioCICE <zetoxmagic@gmail.com>
2019-03-03 13:34:21 +0100

    Ejercicios Fundamentos de Java

---
+++ Fundamentos/Ejercicio0-HolaMundo/src/com/saguadopro/Main.java
@@ -0,0 +1,22 @@
+package com.saguadopro;
+
+/**
+ * Ejercicio - 0
+ *
+ * Pintar por la consola del IDE el mensaje “Hola Mundo JAVA!!”
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        //Para imprimir por la consola, incluye salto de linea
+        System.out.println("Hola mundo JAVA!!!");
+
+        //Imprimir sin salto de linea
+        System.out.print("En este caso no hay salto de linea. ");
+        System.out.print("Por eso esto se imprime a continuación ");
+
+
+
+    }
+}
